502c9f3dd14efc17b112bc82609f85e803865824
Gabriel Bellon <gabriel1490@gmail.com>
2015-12-21 11:35:51 -0200

    Funcionou o?

--- app.rb
+++ app.rb
@@ -1,28 +1,38 @@
 require 'sinatra'
+require 'json'
 require 'nokogiri'
 require 'open-uri'
 
-FEED_URL = "http://feeds.feedblitz.com/german-word-of-the-day"
+#return if params[:token] != ENV['SLACK_TOKEN']
 
-get 'gwotd' do
-  get_document
-
-
-end
-
-def get_document_and_parse
-  @doc = Nokogiri::HTML(open(FEED_URL))
+get '/' do
+  @doc = Nokogiri::HTML(open("http://feeds.feedblitz.com/german-word-of-the-day"))
 
   title = @doc.css('title').text
+  word = @doc.xpath("//title")[1].text
   word_type = @doc.css('td')[0].text
   german_sentence = @doc.css('td')[1].text
-  english_sentenct = @doc.css('td')[2].text
+  english_sentence = @doc.css('td')[2].text
 
-  "#{title} -> #{word_type}, "
+  msg = [
+    "#{title}",
+    "Word: #{word}",
+    "Word type: #{word_type}",
+    "German Sentence: #{german_sentence}",
+    "English Sentence: #{english_sentence}"
+  ]
+
+  respond_message msg
 end
 
-def respond_message msg
+def respond_message message
   content_type :json
 
-  {:text => message}.to_json
+  resp = { text: []}
+
+  message.each do | msg |
+    resp[:text] << msg
+  end
+
+  resp.to_json
 end
